2b8465a9cf900a8a3242b7b11688cd2eb06bffce
Dina Razinkova <dinarazinkova@gmail.com>
2013-03-31 22:11:29 -0400

    week12

---
+++ hello01/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello01::Application.config.secret_token = '355edafdf5126d50113c9758e6e6991d12982c8f60399310a3f240da88841e6a14d3503c7b4ac24b0a742237ebd8b2819c6562e1c9034dac191fd7b80f51632a'
